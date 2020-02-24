a4f86cf6598ba7ed9dfb99d83fa580c3904cf915
Xuehui Ye <yexuehui@momenta.ai>
2020-02-24 22:45:53 +0800

    update tests

---
+++ src/a.ts
@@ -0,0 +1,6 @@
+import { Greeter } from '@/index';
+//import { Greeter } from '../index';
+test('My Greeter', () => {
+  expect(Greeter('Carl')).toBe('Hello Carl');
+  expect('b=2&a=1'.parseStr()).toEqual({a:'1','b':'2'});
+});
